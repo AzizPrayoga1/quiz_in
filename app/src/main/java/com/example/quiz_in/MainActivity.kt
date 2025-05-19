b88a33cbe4648337e695944e66c73be454e2fad7
Dimas Iyza Raditya <dimas_iyza_raditya@teknokrat.ac.id>
2025-05-19 22:45:03 +0700

    halaman utama aplikasi

--- app/src/main/java/com/example/quiz_in/MainActivity.kt
+++ app/src/main/java/com/example/quiz_in/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.quiz_in
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quiz_in.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        //    definisikan binding
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAbout.setOnClickListener{
+        // membuka aboutactivity
+        val intent = Intent(this,AboutActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
