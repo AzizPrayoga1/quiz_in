c6a6b3374d3ce0a22b5fd2ab77a6a9e886f467f7
Dimas <dimas_iyza_raditya@teknokrat.ac.id>
2025-06-12 16:41:49 +0700

    edit tombol kembali

--- app/src/main/java/com/example/quiz_in/AboutActivity.kt
+++ app/src/main/java/com/example/quiz_in/AboutActivity.kt
@@ -1,16 +1,28 @@
 package com.example.quiz_in
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quiz_in.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_about)
+        val binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnback.setOnClickListener {
+            finish()
+        }
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
