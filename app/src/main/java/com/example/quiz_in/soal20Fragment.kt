9d9c7a92fc4b78cdf06452841deb2970a78d7f66
Dimas <dimas_iyza_raditya@teknokrat.ac.id>
2025-06-20 17:46:11 +0700

    menghapus bundle argument

--- app/src/main/java/com/example/quiz_in/soal20Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal20Fragment.kt
@@ -73,10 +73,6 @@ class soal20Fragment : Fragment() {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             soal20Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
             }
     }
 }
