759e8f8bf25d3a4d36b2471e38dd6baba75725d8
Dimas <dimas_iyza_raditya@teknokrat.ac.id>
2025-06-20 22:25:49 +0700

    membuat var benar

--- app/src/main/java/com/example/quiz_in/soal22Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal22Fragment.kt
@@ -21,6 +21,7 @@ class soal22Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var _binding: FragmentSoal20Binding? = null
     private val binding get() =  _binding!!
+    private var benar = 0
 
 
     override fun onCreateView(
