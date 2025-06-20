84116f81d51c6a7bccaef5cf1461612856c4203e
Dimas <dimas_iyza_raditya@teknokrat.ac.id>
2025-06-20 17:54:21 +0700

    menambahkan jika button di klik lanjut ke soal berikutnya

--- app/src/main/java/com/example/quiz_in/soal21Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal21Fragment.kt
@@ -34,6 +34,16 @@ class soal21Fragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Ambil nilai skor dari fragment sebelumnya
+        benar = arguments?.getInt("benar", 0) ?: 0
+        binding.btnsalah1.setOnClickListener {
+            lanjutKeSoal22()
+        }
+
+        binding.btnbenar1.setOnClickListener {
+            benar += 1
+            lanjutKeSoal22()
+        }
 
     }
 
