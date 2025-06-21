26ba270671bc8060f6bd7bafd9867a93c449d949
Dimas <dimas_iyza_raditya@teknokrat.ac.id>
2025-06-21 16:55:50 +0700

    membuat var benar

--- app/src/main/java/com/example/quiz_in/soal35Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal35Fragment.kt
@@ -22,6 +22,8 @@ class soal35Fragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var _binding: FragmentSoal35Binding? = null
     private val binding get() =  _binding!!
+    private var benar = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
