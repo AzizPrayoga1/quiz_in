a3bdb209de536b5e6376f1357ca684f546964da7
Dimas <dimas_iyza_raditya@teknokrat.ac.id>
2025-06-21 14:48:26 +0700

    membuat fun lanjutkesoal29

--- app/src/main/java/com/example/quiz_in/soal28Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal28Fragment.kt
@@ -41,6 +41,18 @@ class soal28Fragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    private fun lanjutKeSoal29() {
+        val bundle = Bundle().apply {
+            putInt("benar", benar)
+        }
+        val nextFragment = soal29Fragment()
+        nextFragment.arguments = bundle
+
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.container, nextFragment)
+            .commit()
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
