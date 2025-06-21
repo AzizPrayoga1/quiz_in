386fbe57b58c5e1e3fa9774261b508c74d5e9317
Dimas <dimas_iyza_raditya@teknokrat.ac.id>
2025-06-21 13:45:01 +0700

    menambahkan binding

--- app/src/main/java/com/example/quiz_in/soal26Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal26Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.quiz_in.databinding.FragmentSoal25Binding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,8 +19,9 @@ private const val ARG_PARAM2 = "param2"
  */
 class soal26Fragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentSoal25Binding? = null
+    private val binding get() =  _binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +32,8 @@ class soal26Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_soal26, container, false)
+        _binding = FragmentSoal25Binding.inflate(inflater,container,false)
+        return binding.root
     }
 
     companion object {
