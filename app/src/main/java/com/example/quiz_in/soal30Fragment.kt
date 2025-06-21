2e1055a2d95f42fde353a166f57c6d17eb27672d
Dimas <dimas_iyza_raditya@teknokrat.ac.id>
2025-06-21 15:18:42 +0700

    membuat event button

--- app/src/main/java/com/example/quiz_in/soal30Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal30Fragment.kt
@@ -39,6 +39,15 @@ class soal30Fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnsalah1.setOnClickListener {
+            lanjutKeSoal31()
+        }
+
+        binding.btnbenar1.setOnClickListener {
+            benar += 1
+            lanjutKeSoal31()
+        }
     }
 
     private fun lanjutKeSoal31() {
