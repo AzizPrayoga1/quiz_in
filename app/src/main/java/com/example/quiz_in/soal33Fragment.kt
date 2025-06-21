eab550408324beba78380157d20976936d04a2f5
Dimas <dimas_iyza_raditya@teknokrat.ac.id>
2025-06-21 16:26:21 +0700

    membuat fun onviewcreated

--- app/src/main/java/com/example/quiz_in/soal33Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal33Fragment.kt
@@ -37,6 +37,10 @@ class soal33Fragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
