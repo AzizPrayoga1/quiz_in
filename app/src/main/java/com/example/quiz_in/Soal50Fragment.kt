204c3c0cc4c0f979f3bcbeacc3e8393db570cf9d
alexameyaksyah <alexameyaksyah@gmail.com>
2025-06-22 16:37:32 +0700

    Update Layout soal50

--- app/src/main/java/com/example/quiz_in/Soal50Fragment.kt
+++ app/src/main/java/com/example/quiz_in/Soal50Fragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.quiz_in.databinding.FragmentSoal20Binding
+import com.example.quiz_in.databinding.FragmentSoal50Binding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,25 +18,47 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Soal50Fragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Soal50Fragment : Fragment() {
+class soal50Fragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentSoal50Binding? = null
+    private val binding get() =  _binding!!
+    private var benar = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_soal50, container, false)
+        _binding = FragmentSoal50Binding.inflate(inflater,container,false)
+        return binding.root
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Ambil nilai skor dari fragment sebelumnya
+        benar = arguments?.getInt("benar", 0) ?: 0
+        binding.btnsalah1.setOnClickListener {
+            lanjutKeSoal51()
+        }
+
+        binding.btnbenar1.setOnClickListener {
+            benar += 1
+            lanjutKeSoal51()
+        }
+    }
+    private fun lanjutKeSoal51() {
+        val bundle = Bundle().apply {
+            putInt("benar", benar)
+        }
+        val nextFragment = soal51Fragment()
+        nextFragment.arguments = bundle
+
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.container, nextFragment)
+            .commit()
     }
 
     companion object {
