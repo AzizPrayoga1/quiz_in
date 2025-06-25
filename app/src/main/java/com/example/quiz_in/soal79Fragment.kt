c0886e45f45d849bd052300365892128120622fe
aziz <prayogaaziz873@gmail.com>
2025-06-25 21:34:14 +0700

    membuat fun onviewcreated

--- app/src/main/java/com/example/quiz_in/soal79Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal79Fragment.kt
@@ -37,6 +37,11 @@ class soal79Fragment : Fragment() {
         _binding = FragmentSoal79Binding.inflate(inflater, container, false)
         return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
 
     companion object {
         /**
