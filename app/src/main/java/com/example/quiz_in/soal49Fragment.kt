7983a1b25d2824326ad3496a7531dc79bb397d52
aziz <prayogaaziz873@gmail.com>
2025-06-25 11:07:02 +0700

    memperbaiki error

--- app/src/main/java/com/example/quiz_in/soal49Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal49Fragment.kt
@@ -5,7 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.quiz_in.databinding.FragmentSoal45Binding
+import com.example.quiz_in.databinding.FragmentSoal47Binding
+import com.example.quiz_in.databinding.FragmentSoal48Binding
 import com.example.quiz_in.databinding.FragmentSoal49Binding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -27,7 +28,14 @@ class soal49Fragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSoal49Binding.inflate(inflater,container,false)
+        return binding.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Ambil nilai skor dari fragment sebelumnya
