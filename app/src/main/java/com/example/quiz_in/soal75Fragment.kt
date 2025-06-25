8c93e5ded893667d4e3a01e2acf70f7b4772e485
aziz <prayogaaziz873@gmail.com>
2025-06-25 14:39:01 +0700

    membuat binding

--- app/src/main/java/com/example/quiz_in/soal75Fragment.kt
+++ app/src/main/java/com/example/quiz_in/soal75Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.quiz_in.databinding.FragmentSoal2Binding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,7 +19,8 @@ private const val ARG_PARAM2 = "param2"
  */
 class soal75Fragment : Fragment() {
     // TODO: Rename and change types of parameters
-
+    private var _binding: FragmentSoal2Binding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
